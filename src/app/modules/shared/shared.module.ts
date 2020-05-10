80d9e209318a0c12a3237bcba6de755b9fdd752e
Szymon Standarski <s.standarski@gmail.com>
2020-05-10 07:29:01 +0200

    feat(header): create header example

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {ToolbarModule} from 'primeng/toolbar';
+import {ButtonModule} from 'primeng/button';
 
+const SHARED_MODULES = [
+  CommonModule,
+  ToolbarModule,
+  ButtonModule
+]
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    ...SHARED_MODULES
+  ],
+  exports: [
+    ...SHARED_MODULES
   ]
 })
 export class SharedModule { }
