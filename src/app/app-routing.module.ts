4f96dea59fd88c0a8499b9fb1a5c2be7e3814c33
Szymon Standarski <s.standarski@gmail.com>
2020-05-15 15:56:59 +0200

    feat(reports): add routing to reporst pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,17 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: '',
+    redirectTo: '/reports',
+    pathMatch: 'full'
+  },
+  {
     path: 'user',
-    loadChildren: () =>
-      import('./modules/user/user.module').then((m) => m.UserModule),
+    loadChildren: () => import('./modules/user/user.module').then((m) => m.UserModule),
+  },
+  {
+    path: 'reports',
+    loadChildren: () => import('./modules/reports/reports.module').then((m) => m.ReportsModule),
   },
 ];
 
