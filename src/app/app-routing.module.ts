0db3063f44540d73e72b8ceb5e363b25b4f1cf9e
Szymon Standarski <s.standarski@gmail.com>
2020-05-16 20:55:14 +0200

    feat(routing): add authGuard to raports routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './modules/notes/auth.guard';
 
 const routes: Routes = [
   {
     path: '',
     redirectTo: '/reports',
-    pathMatch: 'full'
+    pathMatch: 'full',
   },
   {
     path: 'user',
@@ -14,11 +15,13 @@ const routes: Routes = [
   {
     path: 'reports',
     loadChildren: () => import('./modules/reports/reports.module').then((m) => m.ReportsModule),
+    canLoad: [AuthGuard],
   },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
+  providers: [AuthGuard],
 })
 export class AppRoutingModule {}
