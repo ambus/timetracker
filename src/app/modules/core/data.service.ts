53aaf8b43487bfd0ec9d2d96d60cb43bba9f28f5
Szymon Standarski <s.standarski@gmail.com>
2020-05-16 20:39:17 +0200

    feat(dataService): create dataService

--- src/app/modules/core/data.service.ts
+++ src/app/modules/core/data.service.ts
@@ -1,9 +1,78 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore/firestore';
+import { AngularFirestoreCollection } from '@angular/fire/firestore/public_api';
+import { DocumentReference } from '@google-cloud/firestore';
+import { BehaviorSubject, Observable, throwError } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+import { Report } from '../shared/models/report';
+import { TimeInfo } from '../shared/models/timeInfo';
+import { AuthService } from './auth.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DataService {
+  protected readonly USERS_COLLECTION = 'users';
+  protected readonly REPORTS_COLLECTION = 'reports';
+  public isLoading$ = new BehaviorSubject<boolean>(true);
 
-  constructor() { }
+  get timestamp() {
+    return new Date().getTime();
+  }
+  constructor(private afDb: AngularFirestore, private auth: AuthService) {}
+
+  getUserReportsCollection(): AngularFirestoreCollection<Report> {
+    return this.afDb.collection<Report & TimeInfo>(`${this.USERS_COLLECTION}/${this.auth.id}/${this.REPORTS_COLLECTION}`, (ref) =>
+      ref.orderBy('updated_at', 'desc')
+    );
+  }
+
+  addReport(report: Report): Promise<DocumentReference<Report>> {
+    const data: Report & TimeInfo = { ...report, created_at: this.timestamp, updated_at: this.timestamp };
+    return (this.getUserReportsCollection().add(data) as unknown) as Promise<DocumentReference<Report>>; //fix types from  angular firestore
+  }
+
+  editReport(report: Report): Promise<void> {
+    return this.getUserReportsCollection()
+      .doc(report.id)
+      .update({
+        ...report,
+        updated_at: this.timestamp,
+      });
+  }
+
+  deleteReport(id: string): Promise<void> {
+    return this.getUserReportsCollection().doc(id).delete();
+  }
+
+  getReport(id: string): Observable<Report> {
+    return this.getUserReportsCollection()
+      .doc<Report>(id)
+      .snapshotChanges()
+      .pipe(
+        map((snapshot) => {
+          const data = snapshot.payload.data();
+          const id = snapshot.payload.id;
+          return { id, ...data };
+        })
+      );
+  }
+
+  getReports(): Observable<Report[]> {
+    return this.getUserReportsCollection()
+      .snapshotChanges()
+      .pipe(
+        map((snapshot) => {
+          return snapshot.map((a) => {
+            const data = a.payload.doc.data();
+            const id = a.payload.doc.id;
+            return { id, ...data };
+          });
+        }),
+        tap((raports) => {
+          this.isLoading$.next(false);
+        }),
+        catchError((e) => throwError(e))
+      );
+  }
 }
