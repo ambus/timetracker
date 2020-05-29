e68b18740a1a13e5aef6612b1ae32dfcac149c02
Szymon Standarski <s.standarski@gmail.com>
2020-05-29 23:17:18 +0200

    fix(reportAdd): add report form for report add

--- src/app/modules/reports/reports-add/reports-add.component.ts
+++ src/app/modules/reports/reports-add/reports-add.component.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Subject } from 'rxjs';
+import { Router } from '@angular/router';
+import { DataService } from '../../core/data.service';
+import { MessageService } from 'primeng/api';
+import { DynamicDialogConfig, DynamicDialogRef } from 'primeng/dynamicdialog';
+import { ReportsFormComponent } from '../reports-form/reports-form.component';
 
 @Component({
   selector: 'app-reports-add',
   templateUrl: './reports-add.component.html',
-  styleUrls: ['./reports-add.component.scss']
+  styleUrls: ['./reports-add.component.scss'],
 })
 export class ReportsAddComponent implements OnInit {
+  @ViewChild(ReportsFormComponent, { static: false }) reportForm: ReportsFormComponent;
+  public userID;
+  public errorMessages$ = new Subject();
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private data: DataService,
+    private messageService: MessageService,
+    public ref: DynamicDialogRef,
+    public config: DynamicDialogConfig
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSaveReport(values) {
+    debugger;
+    this.data
+      .addReport(values)
+      .then((doc) => {
+        this.router.navigate(['./reports']);
+        this.messageService.add({ severity: 'success', summary: 'Add new report', detail: `Report ${doc.id} has been succeffully saved.` });
+      })
+      .catch((e) => {
+        this.errorMessages$.next('Something is wrong when adding to DB');
+      });
   }
 
+  onSendError(message) {
+    this.errorMessages$.next(message);
+  }
+
+  saveReport() {
+    this.onSaveReport(this.reportForm.reportForm.value);
+  }
+
+  cancel() {
+    this.ref.close();
+  }
 }
